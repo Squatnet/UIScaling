8816310a29ce5b0f0ef127fe009ca7c5352ea51a
Matt Abbott <squatn3t@gmail.com>
2023-12-16 22:06:09 +0000

    blah

--- Scale Control.gd
+++ Scale Control.gd
@@ -6,13 +6,14 @@ var current
 func _ready():
 	base = get_tree().root.content_scale_size
 	current = get_tree().root.content_scale_size
-	
-func set_base_size(): # What you think you need to do
+	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS 
+		
+func set_base_size(): # What you think you need to do, however doing this scales the WINDOW SIZE
 
 	var tmp = Vector2i(int(current.x*$Panel/Width.value),int(current.y*$Panel/Height.value))
 	get_tree().root.content_scale_size = tmp
 	current = tmp
-func set_by_multiplier(): # What you actually need to do
+func set_by_multiplier(): # What you actually need to do, as this will multiply the canvasitems (2d) by some value)
 	get_tree().root.content_scale_factor = $Panel/Multiplier.value
 
 func _on_button_pressed():
