17078a7295371c559e6eef6257f1d8ceef6a3c53
Matt Abbott <squatn3t@gmail.com>
2023-12-16 22:02:16 +0000

    Initial commit

---
+++ Scale Control.gd
@@ -0,0 +1,40 @@
+extends Control
+var base 
+var current
+
+
+func _ready():
+	base = get_tree().root.content_scale_size
+	current = get_tree().root.content_scale_size
+	
+func set_base_size(): # What you think you need to do
+
+	var tmp = Vector2i(int(current.x*$Panel/Width.value),int(current.y*$Panel/Height.value))
+	get_tree().root.content_scale_size = tmp
+	current = tmp
+func set_by_multiplier(): # What you actually need to do
+	get_tree().root.content_scale_factor = $Panel/Multiplier.value
+
+func _on_button_pressed():
+	set_base_size()
+	$Panel/Label.text = str(current)
+	
+
+func _on_button_2_pressed():
+	set_by_multiplier()
+	$Panel/Label.text = str(current)
+
+	
+
+
+func _on_width_drag_ended(value_changed):
+	$Panel/W.text = str($Panel/Width.value)
+
+
+
+func _on_height_drag_ended(value_changed):
+	$Panel/H.text = str($Panel/Height.value)
+
+
+func _on_multiplier_drag_ended(value_changed):
+	$Panel/M.text = str($Panel/Multiplier.value)
